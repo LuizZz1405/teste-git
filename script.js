1f9218e3d5d27ed72eb059ac5838075b7bd09627
Luiz Henrique <luizgthub@gmail.com>
2024-04-22 16:14:57 -0300

    inseri a logica de ano de nascimento no codigo

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const name = "Luiz Henrique da Silva Ramos";
-const idade = 18;
+const idade = "19";
+
+const anoNascimento = 2024 - parseInt(idade);
 
 console.log("Olá mundo, me chamo " + name + "e eu tenho " + idade);
+console.log("nasci no ano de: " + anoNascimento);
